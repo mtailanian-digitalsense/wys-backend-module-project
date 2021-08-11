72e739c6c2ca7e18d80a8ff35db2f0806de92f5d
Rodrigo Hernández <rjhernandr@gmail.com>
2021-08-11 10:46:26 -0400

    test

--- main.py
+++ main.py
@@ -557,6 +557,9 @@ def get_layout_gen(project_id, token):
     api_url = f"http://{LAYOUT_MODULE_HOST}:{LAYOUT_MODULE_PORT}" + LAYOUT_MODULE_API + 'inf/'+ str(project_id)
     print('api-url: ',api_url)
     rv = requests.get(api_url, headers=headers)
+    print(rv)
+    print(rv.text)
+    print(rv.status_code)
     if rv.status_code == 200:
         return json.loads(rv.text)
     elif rv.status_code == 500:
