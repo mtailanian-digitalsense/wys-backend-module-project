060017c0465b7e2d08ffc3ee280eb4216752b246
Eduardo Grendi Molina <eduardo.grendi@usm.cl>
2021-12-09 16:47:54 -0300

    prueba con 403 en token

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def token_required(f):
             
         except Exception as err:
           print("test")
-          return jsonify({'message': 'token is invalid', 'error': str(err)}),401
+          return jsonify({'message': 'token is invalid', 'error': str(err)}),403
         except KeyError as kerr:
           return jsonify({'message': 'Can\'t find user_id in token', 'error': str(kerr)})
         user_id: int = data['user_id']
